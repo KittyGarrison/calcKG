17382d8da5e9b913c3e4e01b240a2ca5a671b713
Greg Santikian <gsantikian@gmail.com>
2015-03-05 15:09:26 -0500

    puts userInput in function w/ recursion

--- calc.js
+++ calc.js
@@ -19,31 +19,37 @@ function divide(num1, num2) {
 function sqRoot(num) {
   return Math.sqrt(num);
 }
-var userInput = sget("What operation would you like to perform? \n 1- addition\n 2- subtraction\n 3- multiplication\n 4- division\n 5- square root\n 6- quit\nPlease enter 1-6 ");
 
-switch(userInput.trim()){
-  case "1":
-    console.log("Ok addition.");
-    break;
-  case "2":
-    console.log("Ok subtraction.");
-    break;
-  case "3":
-    console.log("Ok multiplication.");
-    break;
-  case "4":
-    console.log("Ok division.");
-    break;
-  case "5":
-    console.log("Ok square root.");
-    break;
-  case "6":
-    console.log("Goodbye.");
-    break;
-  default:
-    console.log("Invalid input, please try again");
+function operatorChoice(){
+
+  var userInput = sget("What operation would you like to perform? \n 1- addition\n 2- subtraction\n 3- multiplication\n 4- division\n 5- square root\n 6- quit\nPlease enter 1-6 ");
+
+  switch(userInput.trim()){
+    case "1":
+      console.log("Ok addition.");
+      break;
+    case "2":
+      console.log("Ok subtraction.");
+      break;
+    case "3":
+      console.log("Ok multiplication.");
+      break;
+    case "4":
+      console.log("Ok division.");
+      break;
+    case "5":
+      console.log("Ok square root.");
+      break;
+    case "6":
+      console.log("Goodbye.");
+      break;
+    default:
+      console.log("Invalid input, please try again");
+      operatorChoice();
+  }
 }
 
+operatorChoice();
 
 
 
