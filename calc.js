b45f03b85de3fdee58893dbb4ec6a03df583b325
Greg Santikian <gsantikian@gmail.com>
2015-03-05 17:10:45 -0500

    fleshes out swich statement

--- calc.js
+++ calc.js
@@ -20,6 +20,12 @@ function sqRoot(num) {
   return Math.sqrt(num);
 }
 
+function getTwoNumbers(){
+  var num1 = sget("Enter the first number. ");
+  var num2 = sget("Enter the second number.");
+  return [Number(num1), Number(num2)];
+}
+
 
 function operatorChoice(){
 
@@ -27,21 +33,34 @@ function operatorChoice(){
 
   switch(userInput.trim()){
     case "1":
-      console.log("Ok addition.");
+      console.log("Ok, addition.");
+      var twoNumbers = getTwoNumbers();
+      console.log(add(twoNumbers[0],twoNumbers[1]));
+      operatorChoice();
       break;
     case "2":
-      console.log("Ok subtraction.");
+      console.log("Ok, subtraction.");
+      var twoNumbers = getTwoNumbers();
+      console.log(subtract(twoNumbers[0],twoNumbers[1]));
+      operatorChoice();
       break;
     case "3":
-      console.log("Ok multiplication.");
+      console.log("Ok, multiplication.");
+      var twoNumbers = getTwoNumbers();
+      console.log(mutiply(twoNumbers[0],twoNumbers[1]));
+      operatorChoice();
       break;
     case "4":
-      console.log("Ok division.");
+      console.log("Ok, division.");
+      var twoNumbers = getTwoNumbers();
+      console.log(divide(twoNumbers[0],twoNumbers[1]));
+      operatorChoice();
       break;
     case "5":
-      console.log("Ok square root.");
+      console.log("Ok, square root.");
       var singleNum = sget("Enter a number: ");
-      console.log("The square root of " + singleNum.trim() + " is " + sqRoot(Number(singleNum)) + ".");
+      console.log("The square root of %s is %s.", singleNum.trim(), sqRoot(Number(singleNum)));
+      operatorChoice();
       break;
     case "6":
       console.log("Goodbye.");
